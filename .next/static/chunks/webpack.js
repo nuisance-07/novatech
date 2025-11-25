a62d30739bf918eebb3ffd9505d1eceaf9cb393d
Mwangi121 <collins.mwangi4@student.moringaschool.com>
2025-11-25 18:20:05 +0300

    Remove `/api/products` route and associated server-side API and cookie handling modules.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb3529e949fec91a"; }
+/******/ 		__webpack_require__.h = function() { return "1356b4151bd9ef92"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
